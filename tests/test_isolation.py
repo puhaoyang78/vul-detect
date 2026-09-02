7a9a18d4eb0317fa5771f3db5365e4bed0edf007
puhaoyang78 <puhaoyang781@gmail.com>
2026-09-02 14:45:45 +0800

    Test local CodeBERT baseline defaults

--- tests/test_isolation.py
+++ tests/test_isolation.py
@@ -35,6 +35,8 @@ class IsolationTests(unittest.TestCase):
         self.assertEqual("/home/phy/jdk21", args.java_home)
         self.assertFalse(args.no_joern)
         self.assertFalse(args.refresh)
+        self.assertEqual("/home/PublicData/PHY-data/resource/codebert", args.codebert_path)
+        self.assertEqual(0.5, args.codebert_threshold)
 
 
 if __name__ == "__main__":
